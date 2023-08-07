8ac70fa60ed06bbc6eec9fce7e716f6f32ded811
Valentina <va13ntina@yandex.ru>
2023-08-07 17:24:33 +0300

    add test_capitalize.py

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,7 +1,5 @@
 from capitalize import capitalize
 
-assert capitalize('hello') != 'Hello'
+assert capitalize('hello') == 'Hello'
 
-assert capitalize('') != ''
-
-print('Все тесты пройдены!')
+assert capitalize('') == ''
